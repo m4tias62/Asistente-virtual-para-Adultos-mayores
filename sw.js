491f68d4190b53a8098c98a394652ab08f3488fd
m4tias62 <matiascmaureira3@gmail.com>
2025-08-24 20:21:15 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,16 @@
-// Service Worker para la Tarjeta Experta
-// Versión (v6): invalida cachés previas y toma control al instalar.
-const CACHE = "tarjeta-experta-v6";
+// Service Worker para la Tarjeta Experta (v7)
+const CACHE = "tarjeta-experta-v7";
 const ASSETS = ["./", "./index.html", "./voice-agent.html", "./manifest.json"];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting())
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((k) => k !== CACHE && caches.delete(k))))
+      .then(() => self.clients.claim())
   );
 });
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.map((k) => k !== CACHE && caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(caches.match(event.request).then((r) => r || fetch(event.request)));
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
 });
